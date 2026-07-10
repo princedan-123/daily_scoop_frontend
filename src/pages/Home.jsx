1683186723a86d77e0cae455bcd24b024d7a66db
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2026-07-10 22:05:54 +0100

    reduced roundness of category button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,18 @@ import GuardianNews from "../components/GuardianNews";
 export default function Home() {
   return (
     <div>
-      <section className="font-roboto flex space justify-evenly h-7 cursor-pointer font-bold">
+      <section className="font-roboto flex flex-wrap gap-4 ml-4 mb-4 cursor-pointer font-bold">
         <div className="category-button">Sports</div>
         <div className="category-button">Health</div>
         <div className="category-button">Science</div>
         <div className="category-button">Business</div>
         <div className="category-button">Politics</div>
         <div className="category-button">Entertainment</div>
+        <div className="category-button">Science</div>
+        <div className="category-button">World</div>
+        <div className="category-button">Technology</div>
+        <div className="category-button">Food</div>
+        <div className="category-button">Finance</div>
       </section>
       <NewsFeed />
       <GuardianNews />
